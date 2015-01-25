48c20c8777e139c87161a63d4eebff6accb19935
Manish joshi <joshimaneesh26@gmail.com>
2015-01-25 13:50:11 +0530

    added clear and del from left button

--- calcjs.js
+++ calcjs.js
@@ -6,6 +6,14 @@ function screen(val){
 	document.getElementById('disp').value+=val;
 }
 
+function del_left(){
+	var val = document.getElementById('disp').value;
+	if(val.length==1)
+		showResult("");
+	else
+		document.getElementById('disp').value = val.slice(0,val.length-1);
+}
+
 function calculate(){
 	showResult(eval(document.getElementById('disp').value));
 }
